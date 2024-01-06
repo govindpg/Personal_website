ccf15b93c69d405c56b5a8eb9b0402434999ea27
govind pg <govindpg1234@gmail.com>
2024-01-06 13:23:05 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,48 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
+import { useEffect, useState } from 'react';
+import { HashLoader
+
+} from 'react-spinners';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-  </Routes>
+  const [loading, setLoading] = useState(false);
 
-    </div>
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            width: '100%',
+            margin: '0 auto',
+            backgroundColor: '#262A56',
+          }}
+        >
+          <HashLoader
+
+ color={'#A084DC'} size={100} speedMultiplier={1} />
+        </div>
+      ) : (
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
